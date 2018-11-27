4e4a331276ed857ecae8cc7a02bf664d88a19c63
BondYuriy <bondar.y@i.ua>
2018-11-27 17:48:58 +0200

    adds slider

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,1 +1,22 @@
-/*gitkeep*/
+$(document).ready(function() {
+  $(".owl-carousel").owlCarousel({
+    loop: true,
+    margin: 19,
+    nav: true,
+    responsive: {
+      0: {
+        items: 1
+      },
+      600: {
+        items: 3
+      },
+      1200: {
+        items: 4
+      }
+    }
+  });
+});
+
+$(".flip").click(function() {
+  $(this).parents('.card').toggleClass("flipped");
+});
