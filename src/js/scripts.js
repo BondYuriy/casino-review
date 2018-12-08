ae7e789a4f94d69e4c3f33d8aec1098e39271cce
BondYuriy <bondar.y@i.ua>
2018-12-08 19:28:20 +0200

    adds adaptive 992, 768

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -7,29 +7,10 @@ $(document).ready(function() {
       0: {
         items: 1
       },
-      600: {
+      768: {
         items: 3
       },
       1200: {
-        items: 4
-      }
-    }
-  });
-});
-
-$(document).ready(function() {
-  $(".owl-carousel2").owlCarousel({
-    loop: true,
-    margin: 19,
-    nav: true,
-    responsive: {
-      0: {
-        items: 1
-      },
-      600: {
-        items: 3
-      },
-      1000: {
         items: 4
       }
     }
