2e9cb640c97e49970a6b3dbf454bbef0630591dc
BondYuriy <bondar.y@i.ua>
2018-12-09 21:36:51 +0200

    adds home page adaptive

--- build/js/scripts.js
+++ build/js/scripts.js
@@ -25,26 +25,6 @@ $(".flip").click(function() {
     .toggleClass("flipped");
 });
 
-//================================================
-
-// $(document).ready(function() {
-//   $(".promo-info").hide();
-
-//   $(".btn-white").click(function() {
-//     var lable = $(".btn-white")
-//       .text()
-//       .trim();
-
-//     if (lable == "READ MORE") {
-//       $(".btn-white").text("READ LESS");
-//       $(".promo-info").show();
-//     } else {
-//       $(".btn-white").text("READ MORE");
-//       $(".promo-info").hide();
-//     }
-//   });
-// });
-
 //====================================================
 
 $(document).ready(function() {
@@ -60,5 +40,51 @@ $(document).ready(function() {
       $(".btn-white").text("READ MORE");
       $(".promo-info").addClass("hidden");
     }
+  });
+});
+
+//=================================================
+
+$(".btn-collapse_table").click(function() {
+  $(this)
+    .prev(".table-collapse_group")
+    .toggle()
+    .toggleClass("class-table");
+
+  $(this)
+    .parents(".table-infos")
+    .toggleClass("class-table_padding");
+});
+
+//==================================================
+
+$(document).ready(function() {
+  var readMore = $(".read-more").html();
+  var lessText = readMore.substr(0, 220);
+
+  if (readMore.length > 220) {
+    $(".read-more")
+      .html(lessText)
+      .append("<a href='#' class='read-more-link'>... Read more</a>");
+  } else {
+    $(".read-more").html(readMore);
+  }
+
+  $("body").on("click", ".read-more-link", function(event) {
+    event.preventDefault();
+
+    $(this)
+      .parent(".read-more")
+      .html(readMore)
+      .append("<a href='#' class='show-less-link'>Read less</a>");
+  });
+
+  $("body").on("click", ".show-less-link", function(event) {
+    event.preventDefault();
+
+    $(this)
+      .parent(".read-more")
+      .html(readMore.substr(0, 220))
+      .append("<a href='' class='read-more-link'>... Read more</a>");
   });
 });
