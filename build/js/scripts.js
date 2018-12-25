0732bafb4254abdd4ee0438b33f008b5d7d1a6dc
BondYuriy <bondar.y@i.ua>
2018-12-25 18:39:32 +0200

    adds carousel1

--- build/js/scripts.js
+++ build/js/scripts.js
@@ -1,20 +1,35 @@
-$(document).ready(function() {
-  $(".owl-carousel").owlCarousel({
-    loop: true,
-    margin: 19,
-    nav: true,
-    responsive: {
-      0: {
-        items: 1
-      },
-      768: {
-        items: 3
-      },
-      1200: {
-        items: 4
-      }
+$(".carousel-1").owlCarousel({
+  loop: true,
+  margin: 10,
+  nav: true,
+  responsive: {
+    0: {
+      items: 1
+    },
+    600: {
+      items: 3
+    },
+    1200: {
+      items: 4
     }
-  });
+  }
+});
+
+$(".carousel-3").owlCarousel({
+  loop: true,
+  margin: 19,
+  nav: true,
+  responsive: {
+    0: {
+      items: 1
+    },
+    768: {
+      items: 3
+    },
+    1200: {
+      items: 4
+    }
+  }
 });
 
 //================================================
